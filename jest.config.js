bc7f00d38e3238f4d6a20eab1bace9de8f360a10
Thiago Costa <tjthiagocosta@gmail.com>
2022-03-21 09:55:58 -0300

    feat: add SignUp Factory

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,5 @@
 module.exports = {
   roots: ['<rootDir>/src'],
-  collectCoverageFrom: ['<rootDir>/src/**/*.ts'],
   coverageDirectory: 'coverage',
   testEnvironment: 'node',
   preset: '@shelf/jest-mongodb',
